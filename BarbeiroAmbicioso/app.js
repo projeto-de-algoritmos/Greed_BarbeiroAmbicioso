35870a53250e6b9ae819f632500a26f8f2401a0e
Daniel Alves <danielribeiroalves0@gmail.com>
2022-03-02 21:34:07 -0300

    Adicionando dependências, arrumando intervalScheduling e configurando frontend.

--- BarbeiroAmbicioso/app.js
+++ BarbeiroAmbicioso/app.js
@@ -2,15 +2,20 @@ const express = require('express');
 const app = express();
 const intervalScheduling = require('./intervalScheduling/intervalScheduling.js');
 app.use(express.static('public'));
+const bodyParser = require('body-parser');
 
 app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/img', express.static(__dirname + 'public/img'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('', function (req, resposta) {
     resposta.sendFile(__dirname + "/public/index.html");
 
 });
+app.set('view engine', 'pug')
 //teste  ordena horarios
+
 var horarios = [{
     inicio: 1,
     fim: 3
@@ -43,6 +48,14 @@ console.log(horarios);
 //fim teste
 var interval = new intervalScheduling()
 console.log(interval.calculaScheduling(horarios.length, horarios))
+horariosScheduling = interval.calculaScheduling(horarios.length, horarios)
+console.log(horariosScheduling[0].inicio)
+app.post("/checkDisponibility", function (req, res) {
+    res.render('../views/test', {
+
+        teste: horariosScheduling
+    });
+});
 
 
 app.listen(3000, () => console.log("Tudo certo"));
--- BarbeiroAmbicioso/intervalScheduling/intervalScheduling.js
+++ BarbeiroAmbicioso/intervalScheduling/intervalScheduling.js
@@ -1,18 +1,18 @@
 
 module.exports = class IntervalScheduling {
     constructor() {
-      
+
     }
-   
-    
+
+
     calculaScheduling(qtd, horarios) {
-      
-       var max = [];
+
+        var max = [];
         var anterior = 0
-    
+
         for (var i = 0; i < qtd; i++) {
             if (horarios[i].inicio >= anterior) {
-                max.push(i)
+                max.push(horarios[i])
                 anterior = horarios[i].fim
             }
         }
